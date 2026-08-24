09d5a2dc654d3e8a1b6fbfecd6e83583decb20f3
jacob <jacobdhartzell@gmail.com>
2026-08-24 10:01:46 -0400

    Complete

--- Exercise.java
+++ Exercise.java
@@ -6,9 +6,9 @@ Purpose: The purpose of this program is to solve the quadratic formula based on 
 and return the roots back to the user
 */
 
-import java.util.Scanner; 
+import java.util.Scanner; // Import Scanner Utility to Read Inputs
 
-public class QuadraticFormulaSolver {
+public class Exercise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Quadratic Formula Solver ");
@@ -16,24 +16,24 @@ public class QuadraticFormulaSolver {
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
+        // Prompt the User for Input of A, B, C.
+
 
         double discriminantSolved = Math.pow(b,2) - (4*a*c);
         double r1 = (-b + Math.pow(discriminantSolved,0.5))/ (2 * a);
         double r2 = (-b - Math.pow(discriminantSolved,0.5))/ (2 * a);
+        // Solve for the discriminant and Roots.
 
 
-        if (discriminantSolved < 0){
-            System.out.println("The equation has no real roots");
-        }
-
+        // Displays output based on what the discriminant is.
+        if (discriminantSolved < 0){ 
+            System.out.println("The equation has no real roots");}
         if (discriminantSolved > 0){
-            System.out.println("The roots are " + r1 + " and " + r2);
-        }
+            System.out.println("The roots are " + r1 + " and " + r2);}
         if (discriminantSolved == 0){
-            System.out.println("The root is " + r1);
-        }
+            System.out.println("The root is " + r1);}
 
-        input.close();
+        input.close(); // close input 
     }
     
 }
